2e5d0ff78cc9630f500566bbb57363ed5b8759b1
Keiji, Yoshimi <walf443@gmail.com>
2011-01-30 01:09:13 +0900

    create gh-pages.

--- source/quickstart/single-user.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-use Data::Dumper;
-
-my $teng = do('create-teng-instance.pl')
-    or die $@;
-
-my $user = $teng->single(user => { id => 1 });
-warn Dumper($user->get_columns);
